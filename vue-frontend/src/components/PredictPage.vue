04fd6b0a813ccb2985c7a6d12f4c8c461bef0b70
Bhanu <bhanurana430@gmail.com>
2024-05-28 12:00:31 +0200

    Minor changes - titles

--- vue-frontend/src/components/PredictPage.vue
+++ vue-frontend/src/components/PredictPage.vue
@@ -1,54 +1,55 @@
 <template>
   <div>
     <h1>Predict Page</h1>
-    <router-link to="/"><button class="cta-button">Back 🤛🏼</button></router-link>
+    
 
     <!-- Combined form with dropdowns and sliders -->
     <form @submit.prevent="submitForm" class="form-container">
       <div class="form-group">
-        <label for="dropdown1">Select Option 1:</label>
+        <label for="dropdown1">Passenger Class</label>
         <select id="dropdown1" v-model="dropdown1" required>
           <option disabled value="">Please select one</option>
-          <option value="Option 1A">Option 1A</option>
-          <option value="Option 1B">Option 1B</option>
-          <option value="Option 1C">Option 1C</option>
+          <option value="Option 1A">First</option>
+          <option value="Option 1B">Second</option>
+          <option value="Option 1C">Third</option>
         </select>
       </div>
 
       <div class="form-group">
-        <label for="dropdown2">Select Option 2:</label>
+        <label for="dropdown2">Sex</label>
         <select id="dropdown2" v-model="dropdown2" required>
           <option disabled value="">Please select one</option>
-          <option value="Option 2A">Option 2A</option>
-          <option value="Option 2B">Option 2B</option>
-          <option value="Option 2C">Option 2C</option>
+          <option value="Option 2A">Male</option>
+          <option value="Option 2B">Female</option>
+          <option value="Option 2C">Other</option>
         </select>
       </div>
 
       <div class="form-group">
-        <label for="dropdown3">Select Option 3:</label>
+        <label for="dropdown3">Embarked</label>
         <select id="dropdown3" v-model="dropdown3" required>
           <option disabled value="">Please select one</option>
-          <option value="Option 3A">Option 3A</option>
-          <option value="Option 3B">Option 3B</option>
-          <option value="Option 3C">Option 3C</option>
+          <option value="Option 3A">Cherbourg</option>
+          <option value="Option 3B">Queenstown</option>
+          <option value="Option 3C">Southampton</option>
         </select>
       </div>
 
       <div class="form-group">
-        <label for="slider1">Select a value (1-50):</label>
+        <label for="slider1">Age (1-100):</label>
         <input type="range" id="slider1" v-model.number="slider1" min="1" max="50" required>
         <span>{{ slider1 }}</span>
       </div>
 
       <div class="form-group">
-        <label for="slider2">Select a value (1-50):</label>
+        <label for="slider2">Fare $ (1-1000):</label>
         <input type="range" id="slider2" v-model.number="slider2" min="1" max="50" required>
         <span>{{ slider2 }}</span>
       </div>
 
       <button type="submit" class="cta-button">Submit</button>
     </form>
+    <router-link to="/"><button class="cta-button">Back 🤛🏼</button></router-link>
   </div>
 </template>
 
